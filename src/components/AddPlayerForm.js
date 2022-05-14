2bcb08d4e4b45630cb9786b2868c4ef13f3ad04d
Linda Marko <lindamarko83@gmail.com>
2022-05-14 09:33:51 +0200

    Add refs to AddPlayerForm

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,34 +1,24 @@
 import React, { Component } from 'react';
 
-class AddPlayerForm extends Component {
-  state = {
-    value: '',
-  };
-
-  handleValueChange = (e) => {
-    this.setState({ value: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    const { addPlayer } = this.props;
+const AddPlayerForm = ({ addPlayer }) => {
+  let playerInput = React.createRef();
+  let handleSubmit = (e) => {
     e.preventDefault();
-    addPlayer(this.state.value);
-    this.setState({ value: '' });
+    addPlayer(playerInput.current.value);
+    e.currentTarget.reset();
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleValueChange}
-          placeholder="Enter a player's name"
-        />
-        <input type="submit" value="Add player" />
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        ref={playerInput}
+        placeholder="Enter a player's name"
+      />
+
+      <input type="submit" value="Add Player" />
+    </form>
+  );
+};
 
 export default AddPlayerForm;
